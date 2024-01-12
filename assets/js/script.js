fb06c8020c4bcde607b81b2e733e60b701437cf7
enlace-de <prisypau81@gmail.com>
2024-01-12 18:18:58 -0400

    d

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,26 +64,7 @@ function copiarAlPortapapeles() {
 
 favoritoIcon.addEventListener('click', copiarAlPortapapeles);
 
-// animacion logo
-// Obtiene la referencia a la imagen por su ID
 
-const logoImage = document.getElementById('logoImage');
-
-// Función para iniciar la animación
-function iniciarAnimacion() {
-    logoImage.classList.add('animacion');
-}
-
-// Agrega un controlador de eventos al cargar la página
-window.addEventListener('load', iniciarAnimacion);
-
-// Agrega un controlador de eventos al hacer clic en la imagen
-logoImage.addEventListener('click', () => {
-    // Reinicia la animación al hacer clic
-    logoImage.classList.remove('animacion');
-    void logoImage.offsetWidth; // Esto fuerza un reinicio de la animación
-    logoImage.classList.add('animacion');
-});
 
 // funcion agregar a pagina de inicio
 
